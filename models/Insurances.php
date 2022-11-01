ecbe9aa62a72925d02ac0654e6666ceee48bfea2
Попов Андрей <duircianos@yandex.ru>
2022-11-01 12:57:48 +0400

    начисление пени

--- models/Insurances.php
+++ models/Insurances.php
@@ -56,14 +56,12 @@ class Insurances extends Core
         $number .= date('y'); // год выпуска полиса
         $number .= '0H3'; // код подразделения выпустившего полис (не меняется)
         $number .= 'NZI'; // код продукта (не меняется)
-        $number .= '165'; // код партнера (не меняется)
-        $number .= 9; // 9ХХХХХХ – номер полиса страхования (первая всегда 9 для экозайма)
+        $number .= '383'; // код партнера (не меняется)
+        $number .= 5; // 9ХХХХХХ – номер полиса страхования (первая всегда 9 для экозайма)
 
-        $id_number = $id;
-        while (strlen($id_number) < 6) {
-            $id_number = '0' . $id_number;
-        }
-        $number .= $id_number;
+        $polis_number = str_pad($id, 7, '0', STR_PAD_LEFT);
+
+        $number .= $polis_number;
 
         return $number;
     }
@@ -197,9 +195,6 @@ class Insurances extends Core
         $id = $this->db->insert_id();
 
         $insurance_number = $this->create_number($id);
-
-        if (!empty($insurance['protection']))
-            $insurance_number = $insurance_number . 'Z';
 
         $this->update_insurance($id, array('number' => $insurance_number));
 
