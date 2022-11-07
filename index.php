5b5877e738aee36157bb59bf4dfed7dccbeaeb82
Андрей Попов <duircianos@yandex.ru>
2022-11-07 15:52:24 +0400

    правки по ОРМ

--- index.php
+++ index.php
@@ -5,35 +5,8 @@ ini_set('display_startup_errors', 'Off');
 
 session_start();
 
-use Illuminate\Database\Capsule\Manager as Capsule;
-
 require 'autoload.php';
 require 'vendor/autoload.php';
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver' => 'mysql',
-    'host' => '127.0.0.1',
-    'database' => 'bf',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-    'collation' => 'utf8_general_ci',
-    'prefix' => '',
-]);
-
-// Set the event dispatcher used by Eloquent models... (optional)
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
-
-$capsule->setEventDispatcher(new Dispatcher(new Container));
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
 
 
 try {
