af030b37cc461e41b943660fbc3b85f84512f7fa
Андрей Попов <duircianos@yandex.ru>
2022-12-18 13:30:54 +0400

    сегменты для ремайндеров

---
+++ cron/RemindersCron.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+chdir(dirname(__FILE__) . '/../');
+
+require 'autoload.php';
+
+class RemindersCron extends Core
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->run();
+    }
+
+    private function run()
+    {
+        RegSegment::sendReminder(1);
+        ActiveSegment::sendReminder(1);
+        PendingSegment::sendReminder(1);
+        RepSegment::sendReminder(1);
+    }
+}
+
+new RemindersCron();
