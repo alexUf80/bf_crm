11ede1dce10a5899fe3791928cd32e5e922db166
Андрей Попов <duircianos@yandex.ru>
2022-12-22 18:17:00 +0400

    сегменты для ремайндеров

--- tools/Onec.php
+++ tools/Onec.php
@@ -4,58 +4,47 @@ class Onec implements ToolsInterface
 {
     protected static $params;
 
-    public static function request($status)
+    public static function request($contracts)
     {
-        /*
-        $orders = OrdersORM::with('user')
-            ->where('status', $status)
-            ->orderBy('id', 'desc')
-            ->limit(5)
-            ->get();
-
-        */
-
-        $orders = OrdersORM::whereIn('id', [30740, 33062, 33715, 33702, 33722])->get();
-
         $i = 0;
 
-        foreach ($orders as $order) {
-            list($passportSerial, $passportNumber) = explode('-', $order->user->passport_serial);
+        foreach ($contracts as $contract) {
+            list($passportSerial, $passportNumber) = explode('-', $contract->user->passport_serial);
 
             $xml['Справочники'][$i]['Контрагент'] =
                 [
-                    'Наименование' => trim($order->user->lastname . ' ' . $order->user->firstname . ' ' . $order->user->patronymic),
-                    'УИД' => trim($order->user->id),
+                    'Наименование' => trim($contract->user->lastname . ' ' . $contract->user->firstname . ' ' . $contract->user->patronymic),
+                    'УИД' => trim($contract->user->id),
                     "ВидКонтрагента" => 'ФизЛицо',
                     'СерияПаспорта' => trim($passportSerial),
                     'НомерПаспорта' => trim($passportNumber),
-                    'ДатаРождения' => date('Y-m-d', strtotime($order->user->birth)),
-                    'КемВыданПаспорт' => trim($order->user->passport_issued . ' ' . date('Y-m-d', strtotime($order->user->passport_date))),
-                    'КодПодразделения' => trim($order->user->subdivision_code),
-                    'МестоРождения' => trim($order->user->birth_place),
-                    'Пол' => ($order->user->gender == 'female') ? 'Ж' : 'М',
-                    'СотовыйТелефон' => trim($order->user->phone_mobile),
-                    'Фамилия' => trim($order->user->lastname),
-                    'Имя' => trim($order->user->firstname),
-                    'Отчество' => trim($order->user->patronymic),
-                    'ИндексПоРегистрации' => trim($order->user->regAddress->zip),
-                    'ИндексФактическогоПроживания' => trim($order->user->factAddress->zip),
-                    'РайонОбластьПоРегистрации' => trim($order->user->regAddress->region),
-                    'РайонОбластьФактическогоПроживания' => trim($order->user->factAddress->region),
-                    'ГородПоРегистрации' => trim($order->user->regAddress->city . ' ' . $order->user->regAddress->city_type),
-                    'ГородФактическогоПроживания' => trim($order->user->factAddress->city . ' ' . $order->user->factAddress->city_type),
-                    'УлицаПоРегистрации' => trim($order->user->regAddress->street . ' ' . $order->user->regAddress->street_type),
-                    'УлицаФактическогоПроживания' => trim($order->user->factAddress->street . ' ' . $order->user->factAddress->street_type),
-                    'ДомПоРегистрации' => trim($order->user->regAddress->building),
-                    'ДомФактическогоПроживания' => trim($order->user->factAddress->building),
-                    'КвартираПоРегистрации' => trim($order->user->regAddress->room),
-                    'КвартираФактическогоПроживания' => trim($order->user->factAddress->room),
-                    'ПредставлениеАдресаПоРегистрации' => trim($order->user->regAddress->adressfull),
-                    'ПредставлениеАдресаФактическогоПроживания' => trim($order->user->factAddress->adressfull),
-                    'МестоРаботы' => trim($order->user->workplace),
-                    'РабочийТелефон' => trim($order->user->workphone),
-                    'Email' => trim($order->user->email),
-                    'ДатаСоздания' => date('Y-m-d', strtotime($order->user->created))
+                    'ДатаРождения' => date('Y-m-d', strtotime($contract->user->birth)),
+                    'КемВыданПаспорт' => trim($contract->user->passport_issued . ' ' . date('Y-m-d', strtotime($contract->user->passport_date))),
+                    'КодПодразделения' => trim($contract->user->subdivision_code),
+                    'МестоРождения' => trim($contract->user->birth_place),
+                    'Пол' => ($contract->user->gender == 'female') ? 'Ж' : 'М',
+                    'СотовыйТелефон' => trim($contract->user->phone_mobile),
+                    'Фамилия' => trim($contract->user->lastname),
+                    'Имя' => trim($contract->user->firstname),
+                    'Отчество' => trim($contract->user->patronymic),
+                    'ИндексПоРегистрации' => trim($contract->user->regAddress->zip),
+                    'ИндексФактическогоПроживания' => trim($contract->user->factAddress->zip),
+                    'РайонОбластьПоРегистрации' => trim($contract->user->regAddress->region),
+                    'РайонОбластьФактическогоПроживания' => trim($contract->user->factAddress->region),
+                    'ГородПоРегистрации' => trim($contract->user->regAddress->city . ' ' . $contract->user->regAddress->city_type),
+                    'ГородФактическогоПроживания' => trim($contract->user->factAddress->city . ' ' . $contract->user->factAddress->city_type),
+                    'УлицаПоРегистрации' => trim($contract->user->regAddress->street . ' ' . $contract->user->regAddress->street_type),
+                    'УлицаФактическогоПроживания' => trim($contract->user->factAddress->street . ' ' . $contract->user->factAddress->street_type),
+                    'ДомПоРегистрации' => trim($contract->user->regAddress->building),
+                    'ДомФактическогоПроживания' => trim($contract->user->factAddress->building),
+                    'КвартираПоРегистрации' => trim($contract->user->regAddress->room),
+                    'КвартираФактическогоПроживания' => trim($contract->user->factAddress->room),
+                    'ПредставлениеАдресаПоРегистрации' => trim($contract->user->regAddress->adressfull),
+                    'ПредставлениеАдресаФактическогоПроживания' => trim($contract->user->factAddress->adressfull),
+                    'МестоРаботы' => trim($contract->user->workplace),
+                    'РабочийТелефон' => trim($contract->user->workphone),
+                    'Email' => trim($contract->user->email),
+                    'ДатаСоздания' => date('Y-m-d', strtotime($contract->user->created))
                 ];
 
             $i++;
@@ -87,12 +76,11 @@ class Onec implements ToolsInterface
             $i++;
         }
 
-        foreach ($orders as $order) {
-            $contract = ContractsORM::where('order_id', $order->id)->first();
+        foreach ($contracts as $contract) {
 
             $xml['Документы'][$i]['Сделка'] =
                 [
-                    'ДатаЗайма' => date('Y-m-d', strtotime($order->date)),
+                    'ДатаЗайма' => date('Y-m-d', strtotime($contract->create_date)),
                     'НомерЗайма' => $contract->number,
                     'УИД' => $contract->id,
                     'ПСК' => number_format(round($contract->base_percent * 365, 3), 3, '.', ''),
@@ -100,7 +88,7 @@ class Onec implements ToolsInterface
                     'Подразделение' => 1,
                     'СуммаЗайма' => number_format(round($contract->amount, 2), 2, '.', ''),
                     'ДатаВозврата' => date('Y-m-d', strtotime($contract->return_date)),
-                    'Заемщик' => $order->user->id,
+                    'Заемщик' => $contract->user->id,
                     'Процент' => $contract->base_percent,
                     'ПроцентПриПросрочке' => $contract->base_percent + 0.005,
                     'ТипДокументаРасхода' => 0,
@@ -115,6 +103,8 @@ class Onec implements ToolsInterface
             $promocodes = PromocodesORM::get();
 
             $product = ['КредитныйПродукт' => 1];
+
+            $order = OrdersORM::where('contract_id', $contract->id)->first();
 
             foreach ($promocodes as $promocode) {
                 if ($order->promocode_id == $promocode->id)
