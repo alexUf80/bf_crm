a435ba00b0b2a3a47879b6fccec02b7ebb744e46
Андрей Попов <duircianos@yandex.ru>
2023-03-10 16:48:52 +0400

    reminders cron

--- tools/reminders-segments/ExpireSegment.php
+++ tools/reminders-segments/ExpireSegment.php
@@ -25,6 +25,7 @@ class ExpireSegment extends SegmentsAbstract
 
         $settings = new Settings();
         $limitCommunications = $settings->sms_limit_communications;
+
         $contracts = ContractsORM::where('status', 4)->where('return_date', '>=', date('Y-m-d 00:00:00', strtotime('2023-02-19')))->get();
 
         foreach ($contracts as $contract) {
@@ -32,14 +33,31 @@ class ExpireSegment extends SegmentsAbstract
             $now = new DateTime();
             $returnDate = new DateTime(date('Y-m-d', strtotime($contract->return_date)));
 
-            if(date_diff($now, $returnDate) != $reminder->countTime)
+            if(date_diff($now, $returnDate)->days != $reminder->countTime)
                 continue;
 
             $limitDays = 0;
             $limitWeek = 0;
             $limitMonth = 0;
 
+
+            $communications = CallBotCronORM::where('userId', $contract->user_id)->get();
             $canSend = 1;
+
+            if (!empty($communications)) {
+                foreach ($communications as $communication) {
+                    $created = date('Y-m-d H:i:s', strtotime($communication->created));
+
+                    if ($created >= $thisDayFrom && $created <= $thisDayTo)
+                        $limitDays++;
+
+                    if ($created >= $thisWeekFrom && $created <= $thisWeekTo)
+                        $limitWeek++;
+
+                    if ($created >= $thisMonthFrom && $created <= $thisMonthTo)
+                        $limitMonth++;
+                }
+            }
 
             $communications = RemindersCronORM::where('userId', $contract->user_id)->get();
 
@@ -68,28 +86,14 @@ class ExpireSegment extends SegmentsAbstract
 
             $user = UsersORM::where('id', $contract->user_id)->first();
 
-            if (empty($user->time_zone))
-                continue;
-
-            $clientTime = gmdate('Y-m-d H:i:s', strtotime($user->time_zone));
-
-            $isHoliday = WeekendCalendarORM::where('date', date('Y-m-d'))->first();
-            $settings = new Settings();
-
-            if (!empty($isHoliday) && date('G', strtotime($clientTime)) < $settings->holiday_worktime['from'] && date('G', strtotime($clientTime)) > $settings->holiday_worktime['to'])
-                $canSend = 0;
-
-            if (empty($isHoliday) && date('G', strtotime($clientTime)) < $settings->workday_worktime['from'] && date('G', strtotime($clientTime)) > $settings->workday_worktime['to'])
-                $canSend = 0;
-
-
             if ($canSend == 1) {
                 $reminderLog =
                     [
                         'reminderId' => $reminder->id,
                         'userId' => $user->id,
                         'message' => $reminder->msgSms,
-                        'phone' => $user->phone_mobile
+                        'phone' => $user->phone_mobile,
+                        'orderId' => $contract->order_id
                     ];
 
                 RemindersCronORM::insert($reminderLog);
