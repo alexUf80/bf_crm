f6be77771424fc4a0926b6a37b44bef076d63dd8
alexUf80 <50929505+alexUf80@users.noreply.github.com>
2023-10-20 11:02:04 +0300

    НБКИ показатели ключа extra_scoring отчета Риски

--- models/InsurancesORM.php
+++ models/InsurancesORM.php
@@ -23,16 +23,12 @@ class InsurancesORM extends \Illuminate\Database\Eloquent\Model
 
     // public static function get_insurance_cost($amount)
     // {
-    //     if ($amount <= 3999)
+    //     if ($amount <= 4999)
     //         return 590;
-    //     elseif ($amount >= 4000 && $amount <= 4999)
-    //         return 690;
-    //     elseif ($amount >= 5000 && $amount <= 6999)
+    //     elseif ($amount >= 5000 && $amount <= 8999)
     //         return 890;
-    //     elseif ($amount >= 7000 && $amount <= 10999)
-    //         return 1490;
-    //     elseif ($amount >= 11000)
-    //         return 2190;
+    //     elseif ($amount >= 9000)
+    //         return 990;
     // }
 
     public function get_insurance_cost($amount, $address_id)
@@ -76,16 +72,12 @@ class InsurancesORM extends \Illuminate\Database\Eloquent\Model
             return (float)$insurance_cost_amount;
         }
         else {
-            if ($amount <= 3999)
+            if ($amount <= 4999)
                 return 590;
-            elseif ($amount >= 4000 && $amount <= 4999)
-                return 690;
-            elseif ($amount >= 5000 && $amount <= 6999)
+            elseif ($amount >= 5000 && $amount <= 8999)
                 return 890;
-            elseif ($amount >= 7000 && $amount <= 10999)
-                return 1490;
-            elseif ($amount >= 11000)
-                return 2190;
+            elseif ($amount >= 9000)
+                return 990;
         }
     }
 }
