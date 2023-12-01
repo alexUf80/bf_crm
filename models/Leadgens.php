8aa1f1101e1e6af23a725d68aaa0e044337a472e
alexUf80 <50929505+alexUf80@users.noreply.github.com>
2023-12-01 11:32:12 +0300

    commit

--- models/Leadgens.php
+++ models/Leadgens.php
@@ -193,13 +193,13 @@ class Leadgens extends Core
 
         $link = "https://alianscpa.ru/api/contacts/?phone=$order->phone_mobile&token=cf9eaf664759eb5e6a1d93b41edf85b4&email=$order->email&name=$order->firstname&surname=$order->lastname&patronymic=$order->patronymic&date_birthday=$order->birth&geo=$address_locacity";
         $sas = file_get_contents($this->config->root_dir.'files/sas.txt');
-        file_put_contents($this->config->root_dir.'files/sas.txt',$sas.'---'.$link);
-        // $ch = curl_init($link);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        // curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-        // curl_exec($ch);
-        // curl_close($ch);
+
+        $ch = curl_init($link);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+        curl_exec($ch);
+        curl_close($ch);
 
         return 1;
     }
